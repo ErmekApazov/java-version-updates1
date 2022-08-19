5f63d3dd8f93d69037fda7fc8fbde02a11f81e4f
Ermek Apazov <apazovermek@gmail.com>
2022-08-19 11:59:40 -0400

    Stream Module: StreamOperations updated

--- m6-stream/src/main/java/com/cydeo/StreamOperations.java
+++ m6-stream/src/main/java/com/cydeo/StreamOperations.java
@@ -2,18 +2,37 @@ package com.cydeo;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class StreamOperations {
     public static void main(String[] args) {
 
-        List<Integer> list = Arrays.asList(1,2,3,4,5);
-        list.forEach(x -> System.out.println(x));
+        List<Integer> list = Arrays.asList(1,2,2,3,4,5);
+        //list.forEach(x -> System.out.println(x));
         //or
         list.forEach(System.out::println);
 
         //FILTER
+        System.out.println("---------Filter---------------");
         list.stream()
                 .filter(i -> i%2 ==0) // intermediate operation
                 .forEach(System.out:: println); // terminal operation
+
+        //if you need to continue stream flow
+        // list.stream() and so on.
+
+        System.out.println("----------Distinct---------------");
+        Stream<Integer> str = list.stream()
+                .filter(i ->i%2 ==0)
+                .distinct();
+        str.forEach(System.out::println);
+
+        System.out.println("-----------Limit------------------");
+
+        list.stream()
+                .filter(i ->i%2 ==0)
+                .limit(1)
+                .forEach(System.out::println);
+
     }
 }
